a3988cbe6b4742203af5a18fd2aa37288fc45645
GrilledCheese17 <144989763+GrilledCheese17@users.noreply.github.com>
2023-09-14 12:42:43 +0530

    Create UserMgmt.java

---
+++ src/main/java/com/sampurna/app/service/UserMgmt.java
@@ -0,0 +1,21 @@
+package com.example.web_app.service;
+
+import com.example.web_app.login_app.ViewUsers;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+@Service
+public class UserMgmt {
+    List<ViewUsers> list = new ArrayList<>(Arrays.asList(new ViewUsers("arpita","kol", 52)));
+    public List<ViewUsers> getList()
+    {
+        return list;
+    }
+    public void addUser(ViewUsers user)
+    {
+        list.add(user);
+    }
+}
