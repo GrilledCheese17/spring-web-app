dd39ed9625b0947de62b5c24891442f01f8aaab4
GrilledCheese17 <144989763+GrilledCheese17@users.noreply.github.com>
2023-09-14 12:41:46 +0530

    Create Controller.java

---
+++ src/main/java/com/sampurna/app/Controller.java
@@ -0,0 +1,40 @@
+package com.example.web_app.controller;
+
+import com.example.web_app.login_app.ViewUsers;
+import com.example.web_app.service.UserMgmt;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+public class Controller {
+
+    @RequestMapping("/hello")
+    public String message()
+    {
+        return "Hello World";
+    }
+    @Autowired
+    UserMgmt userMgmt;
+    @RequestMapping("/view")
+    public ViewUsers[] getLoginDetails()
+    {
+        List<ViewUsers> list = new ArrayList<ViewUsers>();
+        list.add(new ViewUsers("sampurna","kolkata",27));
+        list.add(new ViewUsers("avirup","mumbai",24));
+        list.add(new ViewUsers("buo","blr",19));
+        return list.toArray(new ViewUsers[0]);
+
+    }
+
+    @RequestMapping(value="/view/add", method= RequestMethod.POST)
+    public void addUser(@RequestBody ViewUsers user)
+    {
+        userMgmt.addUser(user);
+    }
+}
